235ec93ac9a20e0e19f35b822f8ae9f883351afe
Peter Deme <snypox@gmail.com>
2023-08-30 11:46:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
+variable "image_id" {
+  type = string
+}
+
+output "image_id" {
+  value = image_id
+}
+
 resource "random_string" "random" {
   length           = 24
   special          = true
   override_special = "/@£$"
+}
+
+output "randomstring_result" {
+  value = random_string.random.result
 }
