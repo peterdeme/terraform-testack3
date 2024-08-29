f78782e995c8b33cbe6d4a258d769161220b3911
Peter Deme <snypox@gmail.com>
2024-08-29 13:43:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,25 +16,7 @@ resource "random_string" "random3" {
   override_special = "/@£$"
 }
 
-resource "random_string" "random4" {
-  length           = 8
-  special          = true
-  override_special = "/@£$"
-}
-
-resource "random_pet" "pet1" {
-  length = 6
-}
-
-resource "random_pet" "pet2" {
-  length = 7
-}
-
-output "randomstring_result" {
-  value = random_string.random.result
-}
-
-
-output "VPC_ID" {
-  value = "vp-4535j34j3dd"
-}
+#moved {
+#  from = random_string.random3
+#  to   = random_string.random4
+#}
