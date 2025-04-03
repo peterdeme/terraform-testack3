e43428fb560c640eb187ad1919f0d6ae48e0bcac
Peter Deme <snypox@gmail.com>
2025-04-03 15:15:39 +0200

    Add EKS module

--- main.tf
+++ main.tf
@@ -4,37 +4,7 @@ resource "random_string" "random" {
   override_special = "/@£$"
 }
 
-resource "random_string" "random2" {
-  length           = 7
-  special          = true
-  override_special = "/@£$"
-}
-
-resource "random_string" "random3" {
-  length           = 7
-  special          = true
-  override_special = "/@£$"
-}
-
-resource "random_string" "random4" {
-  length           = 8
-  special          = true
-  override_special = "/@£$"
-}
-
-resource "random_pet" "pet1" {
-  length = 6
-}
-
-resource "random_pet" "pet2" {
-  length = 7
-}
-
-output "randomstring_result" {
-  value = random_string.random.result
-}
-
-
-output "VPC_ID" {
-  value = "vp-4535j34j3dd"
+module "terraform-module" {
+  source  = "aws-eks.spacelift.sh/aws-eks/terraform-module/default"
+  version = "0.2.0"
 }
