a75b5deea8dc3e3912b2d1a2265149864aecb056
Peter Deme <snypox@gmail.com>
2025-04-03 16:07:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,3 @@ resource "random_string" "random" {
   special          = true
   override_special = "/@£$"
 }
-
-module "terraform-module" {
-  source  = "aws-eks.spacelift.sh/aws-eks/terraform-module/default"
-  version = "0.2.0"
-}
