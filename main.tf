00120b855e7d991011734c59efc26dcdeef0c370
Peter Deme <snypox@gmail.com>
2025-09-09 15:39:04 +0200

    All ports open

--- main.tf
+++ main.tf
@@ -1,5 +1,35 @@
-resource "random_string" "random" {
-  length           = 9
-  special          = true
-  override_special = "/@£$"
+provider "aws" {
+  region = "us-west-1"
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Create security group with all ports open
+resource "aws_security_group" "open_all" {
+  name        = "open-all-ports"
+  description = "Security group with all ports open"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "open-all-ports"
+  }
 }
