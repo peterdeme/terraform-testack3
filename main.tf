e6ac4a9e6f3a379bd357e8c5854ca3e4f6e83bd0
Peter Deme <snypox@gmail.com>
2026-02-13 16:07:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
-resource "random_string" "random" {
-  length           = 9
-  special          = true
-  override_special = "/@£$"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
