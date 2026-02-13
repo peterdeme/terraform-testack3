6702bca7357cd36b5b7740fe5b80f04939b778d6
Peter Deme <snypox@gmail.com>
2026-02-13 16:43:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,3 +29,27 @@ output "aws_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet from networking stack"
+}
+
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+data "aws_vpc" "selected" {
+  id = data.aws_subnet.selected.vpc_id
+}
+
+resource "aws_security_group" "app" {
+  name        = "orbit-labs-app-sg"
+  description = "Security group for Orbit Labs app"
+  vpc_id      = data.aws_vpc.selected.id
+
+  tags = {
+    name    = "Orbit Labs App SG"
+    project = "Orbit-labs"
+  }
+}
+
